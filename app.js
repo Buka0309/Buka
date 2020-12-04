12ba4dc01c8c03f4e33ed2935af774c89aa2db7f
Bulgan <bulgan8515@gmail.com>
2020-12-04 19:33:31 +0600

    Tsugluulsan onoogoo nemeh, hold tovchiig ajilluulah

--- app.js
+++ app.js
@@ -29,16 +29,33 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore = roundScore + diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
-    roundScore = 0;
-    document.getElementById("current-" + activePlayer).textContent = 0;
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-    diceDom.style.display = "none";
-
-    // if (activePlayer === 0) {
-    // activePlayer = 1;
-    // } else {
-    // activePlayer = 0;
+    switchToNextPlayer();
   }
 });
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  if (scores[activePlayer] >= 20) {
+    document.getElementById("name-" + activePlayer).textContent =
+      "Yeah, WINNER";
+    document
+      .querySelector(".player-" + activePlayer + "- panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "- panel")
+      .classList.remove("active");
+  } else {
+    switchToNextPlayer();
+  }
+
+  switchToNextPlayer();
+});
+function switchToNextPlayer() {
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  diceDom.style.display = "none";
+}
